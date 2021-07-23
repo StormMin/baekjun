021164901ecc372640200ff178b35d5a2b130409
StormMin <87688358+StormMin@users.noreply.github.com>
2021-07-23 12:32:06 +0900

    클래스2 15829 hashing~

---
+++ 2cB15829.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int N, M, i = 1, j = 1,k,max=0;
+	char  a;
+	long long tmp=1,result=0;
+	scanf("%d", &N);
+	getchar();
+	for (int i = 0; i < N; i++) {
+		scanf("%c", &a);
+		k = a - 'a'+1;
+		result += (k * tmp);
+		tmp *= 31;
+	}
+	result %= 1234567891;
+	printf("%lld", result);
+
+}
