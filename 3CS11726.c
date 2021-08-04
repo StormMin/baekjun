32f26fe788686b1c2dbcdb746842a53934f4d71b
StormMin <87688358+StormMin@users.noreply.github.com>
2021-08-04 20:15:24 +0900

    3클래스 11726 n*2 타일

---
+++ 3CS11726.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+int a[1001];
+int main(void) {
+	int N, M, i = 1;
+	scanf("%d", &N);
+	a[1] = 1;
+	a[2] = 2;
+	if (N < 3) {
+		printf("%d", a[N]);
+	}
+	else {
+		for (int j = 3; j <= N; j++) {
+			a[j] = (a[j - 1] + a[j-2])%10007;
+		}
+
+	printf("%d", a[N]);
+	}
+	return 0;
+}
