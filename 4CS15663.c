b6d2e443cb4f3ea3aeaa7818256de94158fe20eb
StormMin <87688358+StormMin@users.noreply.github.com>
2021-08-22 22:11:57 +0900

    4클래스 15663 N과 M(8)

---
+++ 4CS15663.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+int a[10001],c[10],N,M;
+bool b[10];
+int fun(int x, int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < cnt; i++) {
+			printf("%d ", c[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i <= 10000; i++) {
+		if (a[i] > 0) {
+			a[i]--;
+			c[cnt] = i;
+			fun(i, cnt + 1);
+			a[i]++;
+		}
+	}
+}
+int main()
+{
+	int x;
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &x);
+		a[x]++;
+	}
+	fun(1, 0);
+	return 0;
+}
