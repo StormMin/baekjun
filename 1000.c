5b60afe3a9d3f6feb2659e3783ba607026fd846d
StormMin <87688358+StormMin@users.noreply.github.com>
2021-09-06 22:57:24 +0900

    1000

---
+++ 1000.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+int N, M;
+int main()
+{
+	scanf("%d %d", &N, &M);
+	printf("%d", N + M);
+	return 0;
+}
