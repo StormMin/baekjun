b24af247b212262f1675142ddc5fc543c2cb93db
StormMin <87688358+StormMin@users.noreply.github.com>
2021-09-28 22:50:21 +0900

    시간초과 이지만
    실행은 됩니다

---
+++ 4CG9935.c
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+int tmp;
+char str[1000000];
+bool visited[1000000];
+char bomb[38];
+int check(int i, int k) {
+	if (strlen(str) <= i) return 0;
+	if (k == strlen(bomb)) {
+		return 1;
+	}
+	while (visited[i] != 0) {
+		i++;
+	}
+	visited[i] = 1;
+	if (str[i] == bomb[k]) {
+		tmp = check(i + 1, k + 1);
+	}
+	if (tmp != 1) {
+		visited[i] = 0;
+	}
+}
+int main() {
+	scanf("%s", str);
+	scanf("%s", bomb);
+	for (int i = 0; i < strlen(str); i++) {
+		tmp = 0;
+		if (str[i] == bomb[0] && visited[i] == 0) {
+			check(i, 0);
+			if (tmp == 1) {
+				i = i - strlen(bomb);
+				if (i < 0) i = 0;
+			}
+		}
+	}
+	int k = 0;
+	for (int i = 0; i < strlen(str); i++) {
+		if (visited[i] == 0) {
+			printf("%c", str[i]);
+			k = 1;
+		}
+	}
+	if (k == 0) printf("FRULA");
+
+	return 0;
+}
